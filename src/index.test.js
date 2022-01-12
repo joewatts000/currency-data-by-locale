2da83685709061ec00ab05a9719acff0e4891e37
Joe Watts <squarepeoplesolutions@gmail.com>
2022-01-12 21:27:56 +0000

    fix tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,17 @@
-describe('foo', () => {
-  it('bar', () => {
-    expect(true).toBe(true);
+import getLocationData from './index';
+
+describe('getLocationData', () => {
+  it('should return relevant seperator and currency if valid code passed', () => {
+    expect(getLocationData('ja-JP')).toEqual({
+      currency: '￥',
+      separator: ',',
+    });
+  });
+
+  it('should return US seperator and currency if non val;id code passed', () => {
+    expect(getLocationData(null)).toEqual({
+      separator: '.',
+      currency: '$',
+    });
   });
 });
